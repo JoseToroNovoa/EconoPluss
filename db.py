684185d58fc76713171b8869088c382ff762b8b0
JoseToroNovoa <75849895+JoseToroNovoa@users.noreply.github.com>
2020-12-17 09:23:37 -0500

    Update db.py

--- db.py
+++ db.py
@@ -8,13 +8,10 @@ class registro(BaseModel):
     fecha: str
     nota: str
 
-
 registros = {
     1: registro(id_r=1, valor=50000, tipo_registro="Gasto", clase_registro="Comida", fecha="22-11-2020", nota="Comida restaurante"),
-    2: registro(id_r=2, valor=90000, tipo_registro="Gasto", clase_registro="Entrtenimiento", fecha="23-11-2020", nota="Salida a la playa")
-    
+    2: registro(id_r=2, valor=90000, tipo_registro="Gasto", clase_registro="Entretenimiento", fecha="23-11-2020", nota="Salida a la playa")    
 }
-
 
 def consultar_registro():
     lista_registros= []
@@ -23,9 +20,8 @@ def consultar_registro():
         lista_registros.append(registros[e])
     return lista_registros
 
-
 def crear_registro(registro: registro ):
-    if registro.id_r in registros:
+    if registro.id_r in registros: 
         return False
     else:
         registros[registro.id_r] = registro
