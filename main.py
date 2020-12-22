76567937b8b44aef77dd5421150965fa7512029e
JoseToroNovoa <jose.toronovoa@gmail.com>
2020-12-21 23:34:37 -0500

    hola10

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import  HTTPException
+from fastapi import  FastAPI
+import db
+
+app = FastAPI()
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+ "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+"http://localhost", "http://localhost:8080",
+]
+app.add_middleware(
+CORSMiddleware, allow_origins=origins,
+allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
+
+@app.get("/registros/consultar/")
+async def consultar_registros():
+    registro = db.consultar_registro()
+    return registro
+
+
+@app.post("/registros/crear/")
+async def crear_registro(dato: db.registro):
+    registro_creado= db.crear_registro(dato)
+
+    if registro_creado:
+        return {"mensaje": "Registro creado"}
+    else:
+        raise HTTPException(status_code= 400, detail= "Orden ya existe")
